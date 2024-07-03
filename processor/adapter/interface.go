477d3cc72c7e4b2acb499e5aadc041c28d1b5ead
Oleg Semashko <semashko.olezhka@inbox.ru>
2024-07-03 23:32:45 +0300

    GOL-дописал название мессенджеров

--- processor/adapter/interface.go
+++ processor/adapter/interface.go
@@ -1,6 +1,6 @@
 package adapter
 
-// Adapter интерфейс для обращения к месседжерам
+// Adapter интерфейс для обращения к VK, Telegram
 type Adapter interface {
 	// todo какие общие методы должны быть?
 	//
