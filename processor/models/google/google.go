2b36070e0b936cee3d68871b7d55d364657127c8
RyabininAleck <144491572+RyabininAleck@users.noreply.github.com>
2024-07-13 15:09:18 +0300

    GOL-83

--- processor/models/google/google.go
+++ processor/models/google/google.go
@@ -1,6 +1,9 @@
 package googleModels
 
-import "frendler/common/constants"
+import (
+	"fmt"
+	"time"
+)
 
 type LoginRequest struct {
 	RedirectURL string `json:"redirect_url"`
@@ -14,18 +17,115 @@ type User struct {
 	Locale        string `json:"locale"`
 }
 
-type PhoneNumber struct {
-	ID          int    `json:"id"`
-	FriendID    int    `json:"friend_id"`
-	PhoneNumber string `json:"phone_number"`
-	IsPrimary   bool   `json:"is_primary"`
-	NumberType  string `json:"number_type"`
+type GoogleContactsResponse struct {
+	Connections []Contact `json:"connections"`
+	TotalPeople int       `json:"totalPeople"`
+	TotalItems  int       `json:"totalItems"`
 }
 
-type Tags struct {
-	ID          int                `json:"id"`
-	FriendID    int                `json:"friend_id"`
-	Value       string             `json:"value"`
-	ContentType string             `json:"contentType"`
-	Platform    constants.Platform `json:"platform"`
+type Contact struct {
+	Names []struct {
+		DisplayName string `json:"displayName"`
+		FamilyName  string `json:"familyName"`
+		GivenName   string `json:"givenName"`
+	} `json:"names"` //одно
+	Photos []struct {
+		URL string `json:"url"`
+	} `json:"photos"` // одно
+	Birthdays []struct {
+		Date Date `json:"date"`
+	} `json:"birthdays"` //одно
+	Organizations []struct {
+		Name string `json:"name"`
+	} `json:"organizations"` //одно
+
+	Addresses      []Addresses `json:"addresses"` //несколько
+	EmailAddresses []struct {
+		Value string `json:"value"`
+		Type  string `json:"type"`
+	} `json:"emailAddresses"` //несколько
+	PhoneNumbers []struct {
+		CanonicalForm string `json:"canonicalForm"`
+		Type          string `json:"type"`
+	} `json:"phoneNumbers"` //несколько
+	Biographies []struct {
+		Value       string `json:"value"`
+		ContentType string `json:"contentType"`
+	} `json:"biographies"` //несколько
+	URLs []struct {
+		Value         string `json:"value"`
+		Type          string `json:"type"`
+		FormattedType string `json:"formattedType"`
+	} `json:"urls"` //несколько
+	Memberships []struct {
+		ContactGroupMembership struct {
+			ContactGroupId string `json:"contactGroupId"`
+		} `json:"contactGroupMembership"`
+	} `json:"memberships"` //несколько
+	Events []struct {
+		Date          Date   `json:"date"`
+		Type          string `json:"type"`
+		FormattedType string `json:"formattedType"`
+	} `json:"events"` //несколько
+}
+
+func (c *Contact) GetBirthdateData() *time.Time {
+	if len(c.Birthdays) == 0 {
+		return nil
+	}
+
+	date, _ := c.Birthdays[0].Date.DateToTimeStamp()
+	return date
+}
+
+func (c *Contact) GetOrganization() string {
+	if len(c.Organizations) == 0 {
+		return ""
+	}
+	return c.Organizations[0].Name
+}
+
+func (c *Contact) GetPhoneNumber() string {
+	if len(c.PhoneNumbers) == 0 {
+		return ""
+	}
+	return c.PhoneNumbers[0].CanonicalForm
+}
+
+func (c *Contact) GetPhoto() string {
+	if len(c.Photos) == 0 {
+		return ""
+	}
+	return c.Photos[0].URL
+}
+
+type Addresses struct {
+	FormattedValue string `json:"formattedValue"`
+	Type           string `json:"type"`
+	Country        string `json:"country"`
+	CountryCode    string `json:"countryCode"`
+}
+
+type Date struct {
+	Year  int `json:"year"`
+	Month int `json:"month"`
+	Day   int `json:"day"`
+}
+
+func (date *Date) DateToTimeStamp() (*time.Time, error) {
+
+	if date.Month < 1 || date.Month > 12 {
+		return nil, fmt.Errorf("invalid month: %d", date.Month)
+	}
+	if date.Day < 1 || date.Day > 31 {
+		return nil, fmt.Errorf("invalid day: %d", date.Day)
+	}
+
+	t := time.Date(date.Year, time.Month(date.Month), date.Day, 0, 0, 0, 0, time.UTC)
+
+	if t.Month() != time.Month(date.Month) || t.Day() != date.Day {
+		return nil, fmt.Errorf("invalid date: %v", date)
+	}
+
+	return &t, nil
 }
--- processor/proc/handler/contactUpdate.go
+++ processor/proc/handler/contactUpdate.go
@@ -13,6 +13,7 @@ import (
 	"frendler/common/constants"
 	"frendler/processor/config"
 	"frendler/processor/models"
+	googleModels "frendler/processor/models/google"
 )
 
 func (h *HandlerImpl) GoogleContactUpdate(c echo.Context) error {
@@ -55,7 +56,7 @@ func (h *HandlerImpl) GoogleContactUpdate(c echo.Context) error {
 	//todo тут надо достать контакты из базы, обновить старые, добавить новые
 
 	// Сохранение контактов в базу данных
-	IDs, err := h.DB.CreateFriends(userId, contacts, constants.PlatformGoogle)
+	IDs, err := h.DB.CreateFriendSets(userId, contacts, constants.PlatformGoogle)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": fmt.Sprintf("Failed to save contacts: %s", err.Error())})
 	}
@@ -75,8 +76,7 @@ func GetGoogleToken(socialProfile *models.SocialProfile) (*oauth2.Token, error) 
 
 }
 
-// Вспомогательная функция для запроса контактов пользователя
-func getGoogleContacts(userId int, client *http.Client) ([]*models.Friend, error) {
+func getGoogleContacts(userId int, client *http.Client) ([]*models.Set, error) {
 	resp, err := client.Get("https://people.googleapis.com/v1/people/me/connections?personFields=addresses,ageRanges,biographies,birthdays,calendarUrls,clientData,coverPhotos,emailAddresses,events,externalIds,genders,imClients,interests,locales,locations,memberships,metadata,miscKeywords,names,nicknames,occupations,organizations,phoneNumbers,photos,relations,sipAddresses,skills,urls,userDefined")
 	if err != nil {
 		return nil, err
@@ -87,173 +87,93 @@ func getGoogleContacts(userId int, client *http.Client) ([]*models.Friend, error
 		return nil, fmt.Errorf("failed to get contacts: %s", resp.Status)
 	}
 
-	var contactsResponse GoogleContactsResponse
+	var contactsResponse googleModels.GoogleContactsResponse
 	if err := json.NewDecoder(resp.Body).Decode(&contactsResponse); err != nil {
 		return nil, err
 	}
 
-	var friends []*models.Friend
-	//var addresses []*models.Address
-	//var emails []*models.Email
-	//var phoneNumbers []*models.PhoneNumber
-	//var urls []*models.URL
-	//var note []*models.Note
+	var friendSets []*models.Set
 
 	for _, contact := range contactsResponse.Connections {
-		friend := models.Friend{
+		var friendSet models.Set
+		friendSet.Friend = models.Friend{
 			OwnerID:       int64(userId),
 			GivenName:     contact.Names[0].GivenName,
 			FamilyName:    contact.Names[0].FamilyName,
 			DisplayName:   contact.Names[0].DisplayName,
-			Birthdate:     contact.getBirthdateData(),
-			Organizations: contact.getOrganization(),
-			PhoneNumber:   contact.getPhoneNumber(),
-			AvatarURL:     contact.getPhoto(),
+			Birthdate:     contact.GetBirthdateData(),
+			Organizations: contact.GetOrganization(),
+			PhoneNumber:   contact.GetPhoneNumber(),
+			AvatarURL:     contact.GetPhoto(),
 		}
-		friends = append(friends, &friend)
-		//todo вот тут не проставляется friend_id потому что сам friend еще не в базе и не известен его Id недо не забыть подставить Id по время проброса в базу данных
-		//		for _, address := range contact.Addresses {
-		//
-		//		}
-		//
-		//		address := &models.Address{
-		//			Address:     [],
-		//			IsPrimary:   false,
-		//			AddressType: "",
-		//			Country:     "",
-		//			CountryCode: "",
-		//		}
-		//		addresses = append(addresses,address)
 
+		for _, address := range contact.Addresses {
+			friendSet.Addresses = append(friendSet.Addresses, models.Address{
+				Address:     address.FormattedValue,
+				IsPrimary:   false,
+				AddressType: address.Type,
+				Country:     address.Country,
+				CountryCode: address.CountryCode,
+			})
+		}
+
+		for _, email := range contact.EmailAddresses {
+			friendSet.EmailAddresses = append(friendSet.EmailAddresses, models.Email{
+				Email:     email.Value,
+				EmailType: email.Type,
+			})
+		}
+
+		for _, phone := range contact.PhoneNumbers {
+			friendSet.PhoneNumbers = append(friendSet.PhoneNumbers, models.PhoneNumber{
+				PhoneNumber: phone.CanonicalForm,
+				IsPrimary:   false,
+				NumberType:  phone.Type,
+			})
+		}
+
+		for _, biograph := range contact.Biographies {
+			friendSet.Notes = append(friendSet.Notes, models.Note{
+				Title:     biograph.Value,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				Category:  biograph.ContentType,
+			})
+		}
+
+		for _, event := range contact.Events {
+			eventTime, err := event.Date.DateToTimeStamp() //todo добавить логирование ошибки
+			if err != nil {
+				continue
+			}
+
+			friendSet.Notes = append(friendSet.Notes, models.Note{
+				Title:     event.Type,
+				Content:   eventTime.String(),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				Category:  "event", //todo сделать категорию отдельным типом и сделать для нее варианты в constants
+			})
+		}
+
+		for _, url := range contact.URLs {
+			friendSet.URLs = append(friendSet.URLs, models.URL{
+				URL:            url.Value,
+				URLDescription: url.FormattedType, // todo внимательно проверить что тут все на своих местах
+				URLType:        url.Type,
+			})
+		}
+
+		for _, group := range contact.Memberships {
+			friendSet.Tags = append(friendSet.Tags, models.Tag{
+				Tag:      group.ContactGroupMembership.ContactGroupId,
+				Platform: constants.PlatformGoogle,
+			})
+		}
+
+		friendSets = append(friendSets, &friendSet)
 	}
 
-	// todo преобразовать contacts в []models.Friend
-	return friends, nil
+	return friendSets, nil
 
 }
-
-type Contact struct {
-	Names []struct {
-		DisplayName string `json:"displayName"`
-		FamilyName  string `json:"familyName"`
-		GivenName   string `json:"givenName"`
-	} `json:"names"` //одно
-	Photos []struct {
-		URL string `json:"url"`
-	} `json:"photos"` // одно
-	Birthdays []struct {
-		Date Date `json:"date"`
-	} `json:"birthdays"` //одно
-	Organizations []struct {
-		Name string `json:"name"`
-	} `json:"organizations"` //одно
-
-	Addresses []struct {
-		FormattedValue string `json:"formattedValue"`
-		Type           string `json:"type"`
-		Country        string `json:"country"`
-		CountryCode    string `json:"countryCode"`
-	} `json:"addresses"` //несколько
-	EmailAddresses []struct {
-		Value string `json:"value"`
-		Type  string `json:"type"`
-	} `json:"emailAddresses"` //несколько
-	PhoneNumbers []struct {
-		CanonicalForm string `json:"canonicalForm"`
-		Type          string `json:"type"`
-	} `json:"phoneNumbers"` //несколько
-	Biographies []struct {
-		Value       string `json:"value"`
-		ContentType string `json:"contentType"`
-	} `json:"biographies"` //несколько
-	URLs []struct {
-		Value         string `json:"value"`
-		Type          string `json:"type"`
-		FormattedType string `json:"formattedType"`
-	} `json:"urls"` //несколько
-	Memberships []struct {
-		ContactGroupMembership struct {
-			ContactGroupId string `json:"contactGroupId"`
-		} `json:"contactGroupMembership"`
-	} `json:"memberships"` //несколько
-	Events []struct {
-		Date struct {
-			Year  int `json:"year"`
-			Month int `json:"month"`
-			Day   int `json:"day"`
-		} `json:"date"`
-		Type          string `json:"type"`
-		FormattedType string `json:"formattedType"`
-	} `json:"events"` //несколько
-}
-
-type Date struct {
-	Year  int `json:"year"`
-	Month int `json:"month"`
-	Day   int `json:"day"`
-}
-type GoogleContactsResponse struct {
-	Connections []Contact `json:"connections"`
-	TotalPeople int       `json:"totalPeople"`
-	TotalItems  int       `json:"totalItems"`
-}
-
-func (c *Contact) getBirthdateData() *time.Time {
-	if len(c.Birthdays) == 0 {
-		return nil
-	}
-
-	date, _ := dateToTimeStamp(c.Birthdays[0].Date)
-	return date
-}
-
-func dateToTimeStamp(date Date) (*time.Time, error) {
-	// Проверяем корректность значений месяца и дня
-	if date.Month < 1 || date.Month > 12 {
-		return nil, fmt.Errorf("invalid month: %d", date.Month)
-	}
-	if date.Day < 1 || date.Day > 31 {
-		return nil, fmt.Errorf("invalid day: %d", date.Day)
-	}
-
-	// Пытаемся создать объект time.Time
-	t := time.Date(date.Year, time.Month(date.Month), date.Day, 0, 0, 0, 0, time.UTC)
-
-	// Проверяем, корректна ли созданная дата
-	if t.Month() != time.Month(date.Month) || t.Day() != date.Day {
-		return nil, fmt.Errorf("invalid date: %v", date)
-	}
-
-	return &t, nil
-}
-
-func (c *Contact) getOrganization() string {
-	if len(c.Organizations) == 0 {
-		return ""
-	}
-	return c.Organizations[0].Name
-}
-
-func (c *Contact) getPhoneNumber() string {
-	if len(c.PhoneNumbers) == 0 {
-		return ""
-	}
-	return c.PhoneNumbers[0].CanonicalForm
-}
-func (c *Contact) getPhoto() string {
-	if len(c.Photos) == 0 {
-		return ""
-	}
-	return c.Photos[0].URL
-}
-
-//type complect struct {
-//	Friend models.Friend
-//	Addresses []models.Address
-//	EmailAddresses []models.Email
-//	PhoneNumbers []models.PhoneNumber
-//	Notes []models.Note
-//	URLs []models.URL
-//	Tags []models.Tags
-//
-//}
